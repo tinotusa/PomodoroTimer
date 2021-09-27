ba318f8cc9a10561b86e435a6f7cce1007441e15
Tino Tusaumwe <tusatino@gmail.com>
2021-09-27 19:56:07 +1000

    first commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Tino on 27/9/21.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
